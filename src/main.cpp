cc397335030deb16b839cc57dc0f9924f5162fdb
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-03-23 22:02:43 -0500

    Add debug

--- src/main.cpp
+++ src/main.cpp
@@ -228,8 +228,8 @@ int main() {
           	double car_speed = j[1]["speed"];
 
           	// Previous path data given to the Planner
-          	vector<double> previous_path_x = j[1]["previous_path_x"];
-          	vector<double> previous_path_y = j[1]["previous_path_y"];
+          	auto previous_path_x = j[1]["previous_path_x"];
+          	auto previous_path_y = j[1]["previous_path_y"];
           	// Previous path's end s and d values 
           	double end_path_s = j[1]["end_path_s"];
           	double end_path_d = j[1]["end_path_d"];
@@ -299,8 +299,8 @@ int main() {
           	double ref_yaw = deg2rad(car_yaw);
 
           	if (prev_size < 2) {
-          		double prev_car_x = car_x - cos(car_yaw);
-          		double prev_car_y = car_y - sin(car_yaw);
+          		double prev_car_x = car_x - cos(ref_yaw);
+          		double prev_car_y = car_y - sin(ref_yaw);
 
           		ptsx.push_back(prev_car_x);
           		ptsx.push_back(car_x);
