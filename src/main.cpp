423217ba49838a6ec7c99a2d2bfdb255e0e882b2
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-03-26 21:33:37 -0500

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -161,6 +161,7 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 }
 
 vector<string> get_successor_states(string current_state, int current_lane) {
+	//KLA, KLD, LCL, LCR
     vector<string> states;
     states.push_back("KL");
     if(current_state.compare("KL") == 0) {
@@ -187,6 +188,109 @@ double get_simple_prediction(double s, double vx, double vy, int number_timestep
 	return predicted_s;
 }
 
+void get_traj(double car_x, double car_y, double car_vx, double car_vy, double car_yaw, double car_s, const vector<double> &previous_path_x, const vector<double> &previous_path_y,
+		const vector<double> &map_waypoints_s, const vector<double> &map_waypoints_x, const vector<double> &map_waypoints_y, const int lane, const double ref_vel,
+		vector<double> &next_x_vals, vector<double> &next_y_vals) {
+
+	//sparse vector x, y points
+  	vector<double> ptsx;
+  	vector<double> ptsy;
+
+  	double ref_x = car_x;
+  	double ref_y = car_y;
+  	double ref_yaw = deg2rad(car_yaw);
+
+  	int prev_size = 0;
+  	if (previous_path_x != null) {
+  	  	prev_size = previous_path_x.size();
+		if (prev_size < 2) {
+			double prev_car_x = car_x - cos(car_yaw);
+			double prev_car_y = car_y - sin(car_yaw);
+
+			ptsx.push_back(prev_car_x);
+			ptsx.push_back(car_x);
+
+			ptsy.push_back(prev_car_y);
+			ptsy.push_back(car_y);
+		}
+		else {
+			ref_x = previous_path_x[prev_size - 1];
+			ref_y = previous_path_y[prev_size - 1];
+
+			double ref_x_prev = previous_path_x[prev_size - 2];
+			double ref_y_prev = previous_path_y[prev_size - 2];
+			ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+			ptsx.push_back(ref_x_prev);
+			ptsx.push_back(ref_x);
+
+			ptsy.push_back(ref_y_prev);
+			ptsy.push_back(ref_y);
+		}
+  	}
+  	else {
+		ptsx.push_back(car_x);
+		ptsy.push_back(car_y);
+
+		ref_vel = sqrt(vx*vx + vy*vy);
+  	}
+
+  	vector<double> next_wp0 = getXY(car_s + 30, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+  	vector<double> next_wp1 = getXY(car_s + 60, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+  	vector<double> next_wp2 = getXY(car_s + 90, (2 + 4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+  	ptsx.push_back(next_wp0[0]);
+  	ptsx.push_back(next_wp1[0]);
+  	ptsx.push_back(next_wp2[0]);
+
+  	ptsy.push_back(next_wp0[1]);
+  	ptsy.push_back(next_wp1[1]);
+  	ptsy.push_back(next_wp2[1]);
+
+  	//transform points into car frame of reference
+  	for (int i = 0; i < ptsx.size(); i++) {
+  		double shift_x = ptsx[i] - ref_x;
+  		double shift_y = ptsy[i] - ref_y;
+  		ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw));
+  		ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y * cos(0-ref_yaw));
+  	}
+
+  	tk::spline s;
+
+  	s.set_points(ptsx, ptsy);
+
+  	for (int i = 0; i < prev_size; i++) {
+  		next_x_vals.push_back(previous_path_x[i]);
+  		next_y_vals.push_back(previous_path_y[i]);
+  	}
+
+  	double target_x = 30.0;
+  	double target_y = s(target_x);
+  	double target_dist = sqrt((target_x)*(target_x) + (target_y)*(target_y));
+
+  	double x_add_on = 0.0;
+
+  	for (int i = 1; i <= 50 - prev_size; i++) {
+  		double N = (target_dist / (0.02*ref_vel/2.24));
+  		double x_point = x_add_on + (target_x)/N;
+  		double y_point = s(x_point);
+
+  		x_add_on = x_point;
+
+  		double x_ref = x_point;
+  		double y_ref = y_point;
+
+  		x_point = (x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw));
+  		y_point = (x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw));
+
+  		x_point += ref_x;
+  		y_point += ref_y;
+
+  		next_x_vals.push_back(x_point);
+  		next_y_vals.push_back(y_point);
+  	}
+}
+
 void get_trajectory(double car_x, double car_y, double car_yaw, double car_s, const vector<double> &previous_path_x, const vector<double> &previous_path_y,
 		const vector<double> &map_waypoints_s, const vector<double> &map_waypoints_x, const vector<double> &map_waypoints_y, int lane, double ref_vel,
 		vector<double> &next_x_vals, vector<double> &next_y_vals) {
@@ -202,8 +306,8 @@ void get_trajectory(double car_x, double car_y, double car_yaw, double car_s, co
   	int prev_size = previous_path_x.size();
 
   	if (prev_size < 2) {
-  		double prev_car_x = car_x - cos(car_yaw);  //TODO: check if it should be ref_yaw here
-  		double prev_car_y = car_y - sin(car_yaw);  //TODO: check if it should be ref_yaw here
+  		double prev_car_x = car_x - cos(car_yaw);
+  		double prev_car_y = car_y - sin(car_yaw);
 
   		ptsx.push_back(prev_car_x);
   		ptsx.push_back(car_x);
@@ -280,6 +384,19 @@ void get_trajectory(double car_x, double car_y, double car_yaw, double car_s, co
   		next_x_vals.push_back(x_point);
   		next_y_vals.push_back(y_point);
   	}
+}
+
+double get_collision_cost(const vector<double> &trajectory_x, const vector<double> &trajectory_y, const vector<double> &prediction_x, const vector<double> &prediction_y) {
+	double cost = 0.0;
+	double dist = 0.0;
+	for (int i = 0; i < trajectory_x.size(); i++) {
+		dist = distance(trajectory_x[i], trajectory_y[i], prediction_x[i], prediction_y[i]);
+		if (dist < 10.0) { //avoid trajectories less than 10 meters away from other vehicles
+			cost = 1.0;
+			break;
+		}
+	}
+	return cost;
 }
 
 int main() {
@@ -413,7 +530,9 @@ int main() {
           		ref_vel += 0.224;
           	}
 
-          	get_trajectory(car_x, car_y, car_yaw, car_s, previous_path_x, previous_path_y, map_waypoints_s, map_waypoints_x, map_waypoints_y, lane, ref_vel, next_x_vals, next_y_vals);
+          	get_traj(car_x, car_y, 0.0, 0.0, car_yaw, car_s, previous_path_x, previous_path_y, map_waypoints_s, map_waypoints_x, map_waypoints_y, lane, ref_vel, next_x_vals, next_y_vals);
+
+//          	get_trajectory(car_x, car_y, car_yaw, car_s, previous_path_x, previous_path_y, map_waypoints_s, map_waypoints_x, map_waypoints_y, lane, ref_vel, next_x_vals, next_y_vals);
 
 //          	//sparse vector x, y points
 //          	vector<double> ptsx;
