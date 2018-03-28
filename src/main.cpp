6719a07c5c4ff3c73ec81585645fa5c82ff18722
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-03-27 21:49:41 -0500

    fix bugs

--- src/main.cpp
+++ src/main.cpp
@@ -362,7 +362,7 @@ int get_closest_front_car_in_lane(const vector<vector<double>> sensor_fusion, in
   	return ret;
 }
 
-int get_closest_cars_in_side_lane(const vector<vector<double>> &sensor_fusion, const int lane, const double car_s, const double ref_vel, bool is_right) {
+vector<int> get_closest_cars_in_side_lane(const vector<vector<double>> &sensor_fusion, const int lane, const double car_s, const double ref_vel, bool is_right) {
 	vector<int> ret;
 	double dist_radius = ref_vel * 1.61 * 5.0;   //consider all vehicles within 5 seconds
   	int lane_of_interest = 0;
