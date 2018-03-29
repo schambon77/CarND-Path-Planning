aa2b6a39016c4d0b3d6e61fc3b446331b8416355
schambon77 <30735585+schambon77@users.noreply.github.com>
2018-03-28 22:16:25 -0500

    fix issue in change lane cost

--- src/main.cpp
+++ src/main.cpp
@@ -560,7 +560,7 @@ int main() {
                   	vector<double> car_next_x_vals;
                   	vector<double> car_next_y_vals;
                   	get_trajectory(car_x, car_y, car_yaw, car_s, previous_path_x, previous_path_y, map_waypoints_s, map_waypoints_x, map_waypoints_y, tmp_lane, tmp_ref_vel, car_next_x_vals, car_next_y_vals);
-          			double lane_change_min_cost = 999999.0;
+          			double lane_change_max_cost = 0.0;
           			for (int i = 0; i < side_cars.size(); i++) {
                       	vector<double> tmp_next_x_vals;
                       	vector<double> tmp_next_y_vals;
@@ -570,11 +570,11 @@ int main() {
 						tmp_car_d = sensor_fusion[i][6];
 						get_prediction(tmp_car_vx, tmp_car_vy, tmp_car_s, tmp_car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y, tmp_next_x_vals, tmp_next_y_vals);
 						cost = get_cost(car_next_x_vals, car_next_y_vals, tmp_next_x_vals, tmp_next_y_vals, tmp_ref_vel);
-						if (cost < lane_change_min_cost) {
-							lane_change_min_cost = cost;
+						if (cost > lane_change_min_cost) {
+							lane_change_max_cost = cost;
 						}
           			}
-          			cost = lane_change_min_cost;
+          			cost = lane_change_max_cost;
           		}
 				if (cost < min_cost) {
 					next_state = successor_states[i];
